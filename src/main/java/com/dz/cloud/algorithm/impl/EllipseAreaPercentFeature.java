88ab98e2c6a0a6e1e6496b01dd420a8fea2db6ea
王德中 <wangdezhong_1@163.com>
2019-12-15 22:42:50 +0800

    ellispe area percent去掉与尺寸大小有关的因素

--- src/main/java/com/dz/cloud/algorithm/impl/EllipseAreaPercentFeature.java
+++ src/main/java/com/dz/cloud/algorithm/impl/EllipseAreaPercentFeature.java
@@ -1,7 +1,11 @@
 package com.dz.cloud.algorithm.impl;
 
 import com.dz.cloud.algorithm.intf.IVtFeature;
+import com.dz.cloud.geolib.Circle;
+import com.dz.cloud.geolib.Ellipse;
+import com.dz.cloud.geolib.Rect;
 import com.dz.cloud.model.VectorDrawing;
+import org.locationtech.jts.geom.Geometry;
 
 import java.math.BigDecimal;
 
@@ -13,8 +17,33 @@ public class EllipseAreaPercentFeature implements IVtFeature {
     public BigDecimal calc(VectorDrawing vectorDrawing) {
         BigDecimal totalEllipseArea = BigDecimal.valueOf(0);
         for (int i = 0; i < vectorDrawing.getEllipses().size(); i++) {
-            totalEllipseArea = totalEllipseArea.add(BigDecimal.valueOf(vectorDrawing.getEllipses().get(i).getGeometry().getArea()));
+            Ellipse ellipse = vectorDrawing.getEllipses().get(i);
+            if (!ellipse.getVisible()) {
+                continue;
+            }
+            totalEllipseArea = totalEllipseArea.add(BigDecimal.valueOf(ellipse.getGeometry().getArea()));
         }
-        return totalEllipseArea.divide(vectorDrawing.getArea());
+        BigDecimal totalRectArea = BigDecimal.valueOf(0);
+        for (int i = 0; i < vectorDrawing.getRects().size(); i++) {
+            Rect rect =  vectorDrawing.getRects().get(i);
+            if (!rect.getVisible()) {
+                continue;
+            }
+            totalRectArea = totalRectArea.add(BigDecimal.valueOf(rect.getGeometry().getArea()));
+        }
+        BigDecimal totalCircleArea = BigDecimal.valueOf(0);
+        for (int i = 0; i < vectorDrawing.getCircles().size(); i++) {
+            Circle circle = vectorDrawing.getCircles().get(i);
+            if (!circle.getVisible()) {
+                continue;
+            }
+            totalCircleArea = totalCircleArea.add(BigDecimal.valueOf(circle.getGeometry().getArea()));
+        }
+
+        BigDecimal totalArea = totalEllipseArea.add(totalRectArea.add(totalCircleArea));
+        if (totalArea.equals(BigDecimal.valueOf(0))) {
+            return BigDecimal.valueOf(0);
+        }
+        return totalEllipseArea.divide(totalArea, 6);
     }
 }

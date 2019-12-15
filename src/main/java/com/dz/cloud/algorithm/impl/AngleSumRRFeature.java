969aac2704c238f69c8a2e18875c43e85c47b8d6
王德中 <wangdezhong_1@163.com>
2019-12-15 22:29:23 +0800

    增加一些特征

---
+++ src/main/java/com/dz/cloud/algorithm/impl/AngleSumRRFeature.java
@@ -0,0 +1,23 @@
+package com.dz.cloud.algorithm.impl;
+
+import com.dz.cloud.algorithm.intf.IVtFeature;
+import com.dz.cloud.model.VectorDrawing;
+
+import java.math.BigDecimal;
+
+/**
+    Sum of the angle with rectangle and rectangle
+ */
+public class AngleSumRRFeature implements IVtFeature {
+    @Override
+    public BigDecimal calc(VectorDrawing vectorDrawing) {
+        BigDecimal totalCircleArea = BigDecimal.valueOf(0);
+        for (int i = 0; i < vectorDrawing.getCircles().size(); i++) {
+            totalCircleArea = totalCircleArea.add(BigDecimal.valueOf(vectorDrawing.getCircles().get(i).getGeometry().getArea()));
+        }
+        // 直线与直线
+        // 矩形与直线
+        // 矩形与矩形
+        return totalCircleArea.divide(vectorDrawing.getArea());
+    }
+}
